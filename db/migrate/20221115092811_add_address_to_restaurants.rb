2809b47a54af0332542e470908fbf319535e3eb1
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-11-15 09:34:46 +0000

    add address to restaurants

---
+++ db/migrate/20221115092811_add_address_to_restaurants.rb
@@ -0,0 +1,4 @@
+class AddAddressToRestaurants < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_092155) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_092448) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
